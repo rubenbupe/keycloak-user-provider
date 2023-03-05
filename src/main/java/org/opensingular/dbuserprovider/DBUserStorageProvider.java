e789bae24bdb68a145b1057f1865cc4b88b76bf8
Alex <atimofeev1508@gmail.com>
2023-03-05 20:05:38 +0300

    Upgrade to keycloak 20

--- src/main/java/org/opensingular/dbuserprovider/DBUserStorageProvider.java
+++ src/main/java/org/opensingular/dbuserprovider/DBUserStorageProvider.java
@@ -19,16 +19,15 @@ import org.opensingular.dbuserprovider.persistence.DataSourceProvider;
 import org.opensingular.dbuserprovider.persistence.UserRepository;
 import org.opensingular.dbuserprovider.util.PagingUtil;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @JBossLog
 public class DBUserStorageProvider implements UserStorageProvider,
-                                              UserLookupProvider, UserQueryProvider, CredentialInputUpdater, CredentialInputValidator, UserRegistrationProvider {
-    
+        UserLookupProvider, UserQueryProvider, CredentialInputUpdater, CredentialInputValidator, UserRegistrationProvider {
+
     private final KeycloakSession session;
     private final ComponentModel  model;
     private final UserRepository  repository;
@@ -40,103 +39,104 @@ public class DBUserStorageProvider implements UserStorageProvider,
         this.repository = new UserRepository(dataSourceProvider, queryConfigurations);
         this.allowDatabaseToOverwriteKeycloak = queryConfigurations.getAllowDatabaseToOverwriteKeycloak();
     }
-    
-    
-    private List<UserModel> toUserModel(RealmModel realm, List<Map<String, String>> users) {
+
+
+    private Stream<UserModel> toUserModel(RealmModel realm, List<Map<String, String>> users) {
+        log.infov("toUserModel");
         return users.stream()
-                    .map(m -> new UserAdapter(session, realm, model, m, allowDatabaseToOverwriteKeycloak)).collect(Collectors.toList());
+                .map(m -> new UserAdapter(session, realm, model, m, allowDatabaseToOverwriteKeycloak));
     }
-    
-    
+
+
     @Override
     public boolean supportsCredentialType(String credentialType) {
         return PasswordCredentialModel.TYPE.equals(credentialType);
     }
-    
+
     @Override
     public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType) {
         return supportsCredentialType(credentialType);
     }
-    
+
     @Override
     public boolean isValid(RealmModel realm, UserModel user, CredentialInput input) {
-        
+        log.infov("isValid");
         log.infov("isValid user credential: userId={0}", user.getId());
-        
+
         if (!supportsCredentialType(input.getType()) || !(input instanceof UserCredentialModel)) {
             return false;
         }
-        
+
         UserCredentialModel cred = (UserCredentialModel) input;
 
         UserModel dbUser = user;
         // If the cache just got loaded in the last 500 millisec (i.e. probably part of the actual flow), there is no point in reloading the user.)
         if (allowDatabaseToOverwriteKeycloak && user instanceof CachedUserModel && (System.currentTimeMillis() - ((CachedUserModel) user).getCacheTimestamp()) > 500) {
-          dbUser = this.getUserById(user.getId(), realm);
+            dbUser = this.getUserById(realm, user.getId());
 
-          if (dbUser == null) {
-            ((CachedUserModel) user).invalidate();
-            return false;
-          }
+            if (dbUser == null) {
+                ((CachedUserModel) user).invalidate();
+                return false;
+            }
 
-          // For now, we'll just invalidate the cache if username or email has changed. Eventually we could check all (or a parametered list of) attributes fetched from the DB.
-          if (!java.util.Objects.equals(user.getUsername(), dbUser.getUsername()) || !java.util.Objects.equals(user.getEmail(), dbUser.getEmail())) {
-            ((CachedUserModel) user).invalidate();
-          }
+            // For now, we'll just invalidate the cache if username or email has changed. Eventually we could check all (or a parametered list of) attributes fetched from the DB.
+            if (!java.util.Objects.equals(user.getUsername(), dbUser.getUsername()) || !java.util.Objects.equals(user.getEmail(), dbUser.getEmail())) {
+                ((CachedUserModel) user).invalidate();
+            }
         }
         return repository.validateCredentials(dbUser.getUsername(), cred.getChallengeResponse());
     }
-    
+
     @Override
     public boolean updateCredential(RealmModel realm, UserModel user, CredentialInput input) {
-        
+
         log.infov("updating credential: realm={0} user={1}", realm.getId(), user.getUsername());
-        
+
         if (!supportsCredentialType(input.getType()) || !(input instanceof UserCredentialModel)) {
             return false;
         }
-        
+
         UserCredentialModel cred = (UserCredentialModel) input;
         return repository.updateCredentials(user.getUsername(), cred.getChallengeResponse());
     }
-    
+
     @Override
     public void disableCredentialType(RealmModel realm, UserModel user, String credentialType) {
     }
-    
+
     @Override
-    public Set<String> getDisableableCredentialTypes(RealmModel realm, UserModel user) {
-        return Collections.emptySet();
+    public Stream<String> getDisableableCredentialTypesStream(RealmModel realm, UserModel user) {
+        return Stream.empty();
     }
-    
+
     @Override
     public void preRemove(RealmModel realm) {
-        
+
         log.infov("pre-remove realm");
     }
-    
+
     @Override
     public void preRemove(RealmModel realm, GroupModel group) {
-        
+
         log.infov("pre-remove group");
     }
-    
+
     @Override
     public void preRemove(RealmModel realm, RoleModel role) {
-        
+
         log.infov("pre-remove role");
     }
-    
+
     @Override
     public void close() {
         log.debugv("closing");
     }
-    
+
     @Override
-    public UserModel getUserById(String id, RealmModel realm) {
-        
+    public UserModel getUserById(RealmModel realm, String id) {
+        log.infov("getUserById");
         log.infov("lookup user by id: realm={0} userId={1}", realm.getId(), id);
-        
+
         String externalId = StorageId.externalId(id);
         Map<String, String> user = repository.findUserById(externalId);
 
@@ -147,133 +147,110 @@ public class DBUserStorageProvider implements UserStorageProvider,
             return new UserAdapter(session, realm, model, user, allowDatabaseToOverwriteKeycloak);
         }
     }
-    
+
     @Override
-    public UserModel getUserByUsername(String username, RealmModel realm) {
-        
+    public UserModel getUserByUsername(RealmModel realm, String username) {
+        System.out.println("getUserByUsername");
         log.infov("lookup user by username: realm={0} username={1}", realm.getId(), username);
-        
         return repository.findUserByUsername(username).map(u -> new UserAdapter(session, realm, model, u, allowDatabaseToOverwriteKeycloak)).orElse(null);
     }
-    
+
     @Override
-    public UserModel getUserByEmail(String email, RealmModel realm) {
-        
+    public UserModel getUserByEmail(RealmModel realm, String email) {
+        log.infov("getUserByEmail");
         log.infov("lookup user by username: realm={0} email={1}", realm.getId(), email);
-        
-        return getUserByUsername(email, realm);
+        return getUserByUsername(realm, email);
     }
-    
+
     @Override
     public int getUsersCount(RealmModel realm) {
+        log.infov("getUsersCount");
         return repository.getUsersCount(null);
     }
-    
+
     @Override
     public int getUsersCount(RealmModel realm, Set<String> groupIds) {
+        log.infov("getUsersCount");
         return repository.getUsersCount(null);
     }
-    
+
     @Override
     public int getUsersCount(RealmModel realm, String search) {
+        log.infov("getUsersCount");
         return repository.getUsersCount(search);
     }
-    
+
     @Override
     public int getUsersCount(RealmModel realm, String search, Set<String> groupIds) {
+        log.infov("getUsersCount");
         return repository.getUsersCount(search);
     }
-    
+
     @Override
     public int getUsersCount(RealmModel realm, Map<String, String> params) {
+        log.infov("getUsersCount");
         return repository.getUsersCount(null);
     }
-    
+
     @Override
     public int getUsersCount(RealmModel realm, Map<String, String> params, Set<String> groupIds) {
+        log.infov("getUsersCount");
         return repository.getUsersCount(null);
     }
-    
+
     @Override
     public int getUsersCount(RealmModel realm, boolean includeServiceAccount) {
+        log.infov("getUsersCount");
         return repository.getUsersCount(null);
     }
-    
+
     @Override
-    public List<UserModel> getUsers(RealmModel realm) {
-        log.infov("list users: realm={0}", realm.getId());
-        return internalSearchForUser(null, realm, null);
-    }
-    
-    @Override
-    public List<UserModel> getUsers(RealmModel realm, int firstResult, int maxResults) {
-        
+    public Stream<UserModel> searchForUserStream(RealmModel realm, String search, Integer firstResult,
+                                                 Integer maxResults) {
+        log.infov("searchForUserStream");
         log.infov("list users: realm={0} firstResult={1} maxResults={2}", realm.getId(), firstResult, maxResults);
-        return internalSearchForUser(null, realm, new PagingUtil.Pageable(firstResult, maxResults));
-    }
-    
-    @Override
-    public List<UserModel> searchForUser(String search, RealmModel realm) {
-        log.infov("search for users: realm={0} search={1}", realm.getId(), search);
-        return internalSearchForUser(search, realm, null);
-    }
-    
-    @Override
-    public List<UserModel> searchForUser(String search, RealmModel realm, int firstResult, int maxResults) {
-        log.infov("search for users: realm={0} search={1} firstResult={2} maxResults={3}", realm.getId(), search, firstResult, maxResults);
         return internalSearchForUser(search, realm, new PagingUtil.Pageable(firstResult, maxResults));
     }
-    
+
     @Override
-    public List<UserModel> searchForUser(Map<String, String> params, RealmModel realm) {
+    public Stream<UserModel> searchForUserStream(RealmModel realm, Map<String, String> params, Integer firstResult,
+                                                 Integer maxResults) {
+        log.infov("searchForUserStream");
         log.infov("search for users with params: realm={0} params={1}", realm.getId(), params);
         return internalSearchForUser(params.values().stream().findFirst().orElse(null), realm, null);
     }
-    
-    private List<UserModel> internalSearchForUser(String search, RealmModel realm, PagingUtil.Pageable pageable) {
+
+    @Override
+    public Stream<UserModel> searchForUserByUserAttributeStream(RealmModel realm, String attrName, String attrValue) {
+        return Stream.empty();
+    }
+
+    @Override
+    public Stream<UserModel> getGroupMembersStream(RealmModel realm, GroupModel group, Integer firstResult,
+                                                   Integer maxResults) {
+        return Stream.empty();
+    }
+
+    private Stream<UserModel> internalSearchForUser(String search, RealmModel realm, PagingUtil.Pageable pageable) {
+        log.infov("internalSearchForUser");
         return toUserModel(realm, repository.findUsers(search, pageable));
     }
-    
-    @Override
-    public List<UserModel> searchForUser(Map<String, String> params, RealmModel realm, int firstResult, int maxResults) {
-        log.infov("search for users with params: realm={0} params={1} firstResult={2} maxResults={3}", realm.getId(), params, firstResult, maxResults);
-        return internalSearchForUser(params.values().stream().findFirst().orElse(null), realm, new PagingUtil.Pageable(firstResult, maxResults));
-    }
-    
-    @Override
-    public List<UserModel> getGroupMembers(RealmModel realm, GroupModel group, int firstResult, int maxResults) {
-        log.infov("search for group members with params: realm={0} groupId={1} firstResult={2} maxResults={3}", realm.getId(), group.getId(), firstResult, maxResults);
-        return Collections.emptyList();
-    }
-    
-    @Override
-    public List<UserModel> getGroupMembers(RealmModel realm, GroupModel group) {
-        log.infov("search for group members: realm={0} groupId={1} firstResult={2} maxResults={3}", realm.getId(), group.getId());
-        return Collections.emptyList();
-    }
-    
-    @Override
-    public List<UserModel> searchForUserByUserAttribute(String attrName, String attrValue, RealmModel realm) {
-        log.infov("search for group members: realm={0} attrName={1} attrValue={2}", realm.getId(), attrName, attrValue);
-        return Collections.emptyList();
-    }
-    
-    
+
     @Override
     public UserModel addUser(RealmModel realm, String username) {
         // from documentation: "If your provider has a configuration switch to turn off adding a user, returning null from this method will skip the provider and call the next one."
         return null;
     }
-    
-    
+
+
     @Override
     public boolean removeUser(RealmModel realm, UserModel user) {
         boolean userRemoved = repository.removeUser();
-        
+
         if (userRemoved) {
             log.infov("deleted keycloak user: realm={0} userId={1} username={2}", realm.getId(), user.getId(), user.getUsername());
         }
-        
+
         return userRemoved;
     }
 }
--- src/main/java/org/opensingular/dbuserprovider/persistence/UserRepository.java
+++ src/main/java/org/opensingular/dbuserprovider/persistence/UserRepository.java
@@ -24,18 +24,19 @@ import java.util.function.Function;
 
 @JBossLog
 public class UserRepository {
-    
-    
+
+
     private DataSourceProvider  dataSourceProvider;
     private QueryConfigurations queryConfigurations;
-    
+
     public UserRepository(DataSourceProvider dataSourceProvider, QueryConfigurations queryConfigurations) {
         this.dataSourceProvider  = dataSourceProvider;
         this.queryConfigurations = queryConfigurations;
     }
-    
-    
+
+
     private <T> T doQuery(String query, Pageable pageable, Function<ResultSet, T> resultTransformer, Object... params) {
+        log.infov("Query");
         Optional<DataSource> dataSourceOpt = dataSourceProvider.getDataSource();
         if (dataSourceOpt.isPresent()) {
             DataSource dataSource = dataSourceOpt.get();
@@ -61,29 +62,31 @@ public class UserRepository {
         }
         return null;
     }
-    
+
     private List<Map<String, String>> readMap(ResultSet rs) {
+        log.infov("readMap");
+
         try {
             List<Map<String, String>> data         = new ArrayList<>();
             Set<String>               columnsFound = new HashSet<>();
             for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-                String columnLabel = rs.getMetaData().getColumnLabel(i);
-                columnsFound.add(columnLabel);
+                columnsFound.add(rs.getMetaData().getColumnLabel(i));
             }
             while (rs.next()) {
                 Map<String, String> result = new HashMap<>();
                 for (String col : columnsFound) {
-                    result.put(col, rs.getString(col));
+                    result.put(col,rs.getString(col));
                 }
                 data.add(result);
             }
+            log.infov("data {0}", data);
             return data;
         } catch (Exception e) {
             throw new DBUserStorageException(e.getMessage(), e);
         }
     }
-    
-    
+
+
     private Integer readInt(ResultSet rs) {
         try {
             return rs.next() ? rs.getInt(1) : null;
@@ -91,7 +94,7 @@ public class UserRepository {
             throw new DBUserStorageException(e.getMessage(), e);
         }
     }
-    
+
     private Boolean readBoolean(ResultSet rs) {
         try {
             return rs.next() ? rs.getBoolean(1) : null;
@@ -99,7 +102,7 @@ public class UserRepository {
             throw new DBUserStorageException(e.getMessage(), e);
         }
     }
-    
+
     private String readString(ResultSet rs) {
         try {
             return rs.next() ? rs.getString(1) : null;
@@ -107,11 +110,11 @@ public class UserRepository {
             throw new DBUserStorageException(e.getMessage(), e);
         }
     }
-    
+
     public List<Map<String, String>> getAllUsers() {
         return doQuery(queryConfigurations.getListAll(), null, this::readMap);
     }
-    
+
     public int getUsersCount(String search) {
         if (search == null || search.isEmpty()) {
             return Optional.ofNullable(doQuery(queryConfigurations.getCount(), null, this::readInt)).orElse(0);
@@ -120,49 +123,61 @@ public class UserRepository {
             return Optional.ofNullable(doQuery(query, null, this::readInt, search)).orElse(0);
         }
     }
-    
-    
+
+
     public Map<String, String> findUserById(String id) {
-        return Optional.ofNullable(doQuery(queryConfigurations.getFindById(), null, this::readMap, id))
-                       .orElse(Collections.emptyList())
-                       .stream().findFirst().orElse(null);
+        return Optional.ofNullable(doQuery(queryConfigurations.getFindById(), null, this::readMap, Integer.valueOf(id)))
+                .orElse(Collections.emptyList())
+                .stream().findFirst().orElse(null);
     }
-    
+
     public Optional<Map<String, String>> findUserByUsername(String username) {
         return Optional.ofNullable(doQuery(queryConfigurations.getFindByUsername(), null, this::readMap, username))
-                       .orElse(Collections.emptyList())
-                       .stream().findFirst();
+                .orElse(Collections.emptyList())
+                .stream().findFirst();
     }
-    
+
+
     public List<Map<String, String>> findUsers(String search, PagingUtil.Pageable pageable) {
         if (search == null || search.isEmpty()) {
             return doQuery(queryConfigurations.getListAll(), pageable, this::readMap);
         }
-        return doQuery(queryConfigurations.getFindBySearchTerm(), pageable, this::readMap, search);
+        return doQuery(queryConfigurations.getFindBySearchTerm(), pageable, this::readMap, "%" + search + "%");
     }
-    
+
     public boolean validateCredentials(String username, String password) {
+        log.infov("validateCredentials: {0}, {1}", username, password);
+
         String hash = Optional.ofNullable(doQuery(queryConfigurations.getFindPasswordHash(), null, this::readString, username)).orElse("");
+
         if (queryConfigurations.isBlowfish()) {
             return !hash.isEmpty() && BCrypt.checkpw(password, hash);
         } else {
             String hashFunction = queryConfigurations.getHashFunction();
+            log.infov("hashFunction {0}", hashFunction);
 
             if(hashFunction.equals("PBKDF2-SHA256")){
                 String[] components = hash.split("\\$");
                 return new PBKDF2SHA256HashingUtil(password, components[2], Integer.valueOf(components[1])).validatePassword(components[3]);
             }
 
-            MessageDigest digest   = DigestUtils.getDigest(hashFunction);
-            byte[]        pwdBytes = StringUtils.getBytesUtf8(password);
-            return Objects.equals(Hex.encodeHexString(digest.digest(pwdBytes)), hash);
+            MessageDigest digest  = DigestUtils.getDigest(hashFunction);
+
+            String s = Hex.encodeHexString(digest.digest(StringUtils.getBytesUtf8(password)));
+
+            log.infov("Hash from database {0} ", hash);
+            log.infov("Calculated {0}", s);
+
+            boolean res = Objects.equals(s, hash);
+            log.infov("result of hash comparison: {0}", res);
+            return res;
         }
     }
-    
+
     public boolean updateCredentials(String username, String password) {
         throw new NotImplementedException("Password update not supported");
     }
-    
+
     public boolean removeUser() {
         return queryConfigurations.getAllowKeycloakDelete();
     }
